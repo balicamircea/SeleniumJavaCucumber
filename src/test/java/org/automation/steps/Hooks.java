6775bf6b2e0604e5b2395a817932e083c5a1234b
Mircea Balica <mircea.balica@123formbuilder.com>
2025-02-11 15:23:57 +0200

    Added headless and unique user-data-dir

--- src/test/java/org/automation/steps/Hooks.java
+++ src/test/java/org/automation/steps/Hooks.java
@@ -3,10 +3,13 @@ package org.automation.steps;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.Duration;
 import org.automation.utils.Utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hooks {
 
@@ -15,7 +18,20 @@ public class Hooks {
   @Before
   public void setUp() {
     System.out.println("Initializing WebDriver...");
-    driver = new ChromeDriver();
+    Path tempDir;
+    try {
+      tempDir = Files.createTempDirectory("chrome-user-data");
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to create temp directory", e);
+    }
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--disable-gpu");
+    options.addArguments("--remote-allow-origins=*");
+    options.addArguments("--user-data-dir=" + tempDir.toAbsolutePath());
+    driver = new ChromeDriver(options);
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     driver.manage().window().maximize();
   }
